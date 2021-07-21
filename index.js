a7b0a8af32c258d2409cf2cc7226274f669ec669
Admin <hnguyenduchung014@gmail.com>
2021-07-21 21:50:28 +0700

    bt

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();//tao 1 ung dung tu express module
 var port = proces.env.PORT || 5000;
 
 app.listen(port,function (){
-    console.log("Server is running...")
+    console.log("Server is running...");
 });
 // cap quyen truy cap cac  file static (css,img,jquery)
 app.use(express.static("public"));
@@ -33,10 +33,13 @@ var products = [
     }
 ];
 //routing - bo dinh tuyen (nhan vien  cua van phong)
-app.get("/",function (reg,res) {//trang chu
-    //res.send(<html><head></head></html>);//html chi la mot chuoi doi voi may chu
-    //res.sendFile(__dirname+"/public/demobootrap.html");//__dirname -> xuat cho chung ta mot chuoi duong dan chinh xac
-    res.render("home");//tu dong hieu de lay file home.ejs trong thu muc views
+app.get("/",function (req,res){ // trang chu
+    // res.send("<html><head><title</head></html>"); // html- chỉ là 1 chuỗi đối với máy chủ
+    // res.sendFile(__dirname+"/public/demobootstrap.html");  //__dirname -> xuất cho chúng ta 1 chuỗi đường dẫn chính xác trên máy tính đến thư mục này
+    count++;
+    res.render("home",{
+        count: count
+    });// tu dong hieu de lay file home.ejs trong thu muc views
 });
 
 app.get("/ke-toan",function (req,res){
